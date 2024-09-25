daed0ad8ce76856d711c81b34002fb7ac399475d
anilcmp <AnilYadav@cmptechllp.com>
2024-09-25 12:54:27 +0530

    role permission seeder work

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
 }
